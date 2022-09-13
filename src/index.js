26892904b8689c08ce4d0ae6375f01f66a71cfcd
Nam-yuuri <1851061325@e.tlu.edu.vn>
2022-09-13 18:22:14 +0700

    a

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.engine(
     extname: '.hbs',
   }),
 );
-app.set('view engine', 'hbs');
+            app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
