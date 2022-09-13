b41de3bc065a96735d5ea32d71afe0a307604303
Nam-yuuri <1851061325@e.tlu.edu.vn>
2022-09-13 15:44:32 +0700

    kiến thức cốt lõi

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -19,12 +24,22 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  // console.log(req.body)
+  res.send('');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
