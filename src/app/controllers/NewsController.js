2b8ec8d074d2c1780fd9ceb28778711181529bb5
Nam-yuuri <1851061325@e.tlu.edu.vn>
2022-09-13 16:52:41 +0700

    [MVC] Routes & Controllers

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,14 @@
+class NewsController{
+    
+    // GET news
+    index(req, res){
+        res.render('news');
+    }
+
+    // [GET] /mews/:slug
+    show(req, res){
+        res.send('News detail')
+    }
+}
+
+module.exports = new NewsController
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -22,23 +24,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  // console.log(req.body)
-  res.send('');
-});
-
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
